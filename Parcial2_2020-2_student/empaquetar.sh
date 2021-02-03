2e55b2c49e65376e9a58f20b993389a8394e2c23
RubenR <rodriguezmorenorubendario@gmail.com>
2021-02-03 09:21:28 -0500

    1

--- Parcial2_2020-2_student/empaquetar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-zip -r ../$1"_p02.zip" * >/dev/null
-if [ ! -f ../$1"_p02.zip" ]; then
-echo "ERROR!: NO FUE POSIBLE CREAR EL ARCHIVO!."
-else
-echo "¡PERFECTO!, el archivo   '$1_p02.zip'   se creó correctamente :D"
-fi
