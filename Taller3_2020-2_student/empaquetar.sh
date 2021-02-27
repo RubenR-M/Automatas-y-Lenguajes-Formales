1690d31837d0c13813526d8f9d34411487965257
RubenR <rodriguezmorenorubendario@gmail.com>
2021-02-27 14:39:04 -0500

    Taller3_2020-2_student

---
+++ Taller3_2020-2_student/empaquetar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+zip -r ../$1"_t03.zip" * >/dev/null
+if [ ! -f ../$1"_t03.zip" ]; then
+echo "ERROR!: NO FUE POSIBLE CREAR EL ARCHIVO!."
+else
+echo "¡PERFECTO!, el archivo   '$1.zip'   se creó correctamente :D"
+fi
